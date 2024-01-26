c7566fdc7c5e05ecd651cd9042fa0e10d815649d
Tim <timtran007@gmail.com>
2024-01-25 23:12:42 -0600

    fixes dark mode hover for project tags

--- app/components/ProjectTag.jsx
+++ app/components/ProjectTag.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProjectTag({ tag, handleTagChange, isSelected}) {
     const buttonStyles = isSelected
         ? 'bg-gradient-to-br from-green-500 via-blue-400 to-pink-500 font-bold text-white'
-        : 'text-[#ADB7BE] border-[#ADB7BE] hover:border-black hover:text-black'
+        : 'text-[#ADB7BE] border-[#ADB7BE] hover:border-black hover:text-black dark:hover:text-white dark:hover:border-white'
     return (
         <button 
             className={`rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${buttonStyles}`}
