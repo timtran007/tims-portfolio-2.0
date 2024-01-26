a8b8402c3ba7d300f6e62b9b40adefe143092015
Tim <timtran007@gmail.com>
2024-01-25 23:11:50 -0600

    made footer line deeper

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="footer border border-top-[#33353F] border-l-transparent border-r-transparent border-b-transparent">
-        <div className="p-12 flex justify-between items-center">
+    <footer className="footer border-2 border-top-[#33353F] border-l-transparent border-r-transparent border-b-transparent">
+        <div className="py-6 px-12 flex justify-between items-center">
             <Link href={"/"} className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-green-500 via-blue-400 to-pink-500">
               TT
             </Link>
