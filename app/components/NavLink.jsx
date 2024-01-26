4dbbf8e0fc493ca90d62b1e0374015e45ca429e7
Tim <timtran007@gmail.com>
2024-01-25 23:33:20 -0600

    updates text size for nav links

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -4,7 +4,7 @@ export default function NavLink({ href, title}) {
     return(
         <Link
             href={href}
-            className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+            className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-lg rounded md:p-0 hover:text-white"
         >
             {title}
         </Link>
