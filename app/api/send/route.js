df6402c0467735e9f02f854bcaab5e1ea5f3e698
Tim <timtran007@gmail.com>
2024-01-25 22:43:38 -0600

    got the form to work & send emails

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,18 +5,18 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(request, response) {
-    const {body} = request;
-    const {email, subject, message} = body;
   try {
+    const bodyText  = await request.json()
+    const {email, subject, message} = bodyText
     const data = await resend.emails.send({
         from: fromEmail,
         to: ['timtran007@gmail.com'],
         subject: 'Hello world',
         react: (
             <>
-                <h1>{subject}</h1>
-                <p>Person that Submitted the form: {email}</p>
-                <p>Message: {message}</p>
+                <h1>{`${subject}`}</h1>
+                <p>{`Person that Submitted the form: ${email}`}</p>
+                <p>{`Message: ${message}`}</p>
             </>
         ),
     });
